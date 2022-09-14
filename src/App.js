b5b9c5f43cbb6fdb777752c4fc57ae1bfef84f5b
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-09-14 18:29:16 +0100

    Dynamic stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome here";
+  const likes = 50;
+  const link = "https://zubs.github.io";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked by { likes } people as at { (new Date()).toDateString() }</p>
+        <br/>
+        <br/>
+        <br/>
+        <p>Built by <a href={ link } target="_blank">me</a></p>
       </div>
     </div>
   );
