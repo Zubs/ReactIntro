b3d888d1c02a109ac85da01da6f1599b8302887e
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-09-19 02:11:05 +0100

    Added styling

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,9 @@ const title = "Welcome here";
 
 const Home = () => {
 	return (
-		<nav className="navbar">
-			<div className="hom">
-				<h1>{ title }</h1>
-			</div>
-		</nav>
+		<div className="home">
+			<h1>{ title }</h1>
+		</div>
 	);
 }
 
