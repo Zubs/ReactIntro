acbcd9ba11ff0ae6640bbd2bcd345941fd576c9b
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-09-21 01:07:52 +0100

    Removed api tests

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,14 +1,13 @@
 const BlogList = (props) => { // props can be named anything
-	const { blogs, title } = props;
+	const { blogs } = props;
 	const { deleteBlog } = props;
 
 	return (
 		<div className="blog-list">
-			<h2>{ title }</h2>
 			{ blogs.map((blog) => (
 				<div className="blog-preview" key={ blog.id }>
 					<h2>{ blog.title }</h2>
-					<p>Author: { blog?.user?.name }</p>
+					<p>Author: { blog.author }</p>
 					<button onClick={ () => deleteBlog(blog.id) }>Delete</button>
 				</div>
 			)) }
--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,24 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-	const [blogs, setBlogs] = useState([]);
-	const [title, setTitle] = useState(`All ${ blogs.length } Blogs`);
+	const [blogs, setBlogs] = useState([
+		{ id: 1, title: 'Post 1', 'body': 'random long and boring text', author: 'Zubs' },
+		{ id: 2, title: 'Post Two', 'body': 'another long post with no purpose', author: 'Young' },
+		{ id: 3, title: 'Post 111', 'body': 'a final attempt to form a sentence with no actual intent', author: 'Ben Mee' }
+	]);
 
 	const deleteBlog = (id) => {
 		const newBlogs = blogs.filter((blog) => blog.id !== id);
 
 		setBlogs(newBlogs);
-		setTitle(`All ${ blogs.length } Blogs`);
 	}
-
-	useEffect(() => {
-		const fetchBlogs = async () => {
-			let blogs = await fetch('https://jsonplaceholder.typicode.com/posts');
-			blogs = await blogs.json();
-
-			blogs.forEach(async (blog) => {
-				const user = await fetch('https://jsonplaceholder.typicode.com/users/' + blog.userId);
-				blog.user = await user.json();
-			});
-
-			setBlogs(blogs);
-			setTitle(`All ${ blogs.length } Blogs`);
-		}
-
-		fetchBlogs();
-	}, [])
 
 	return (
 		<div className="home">
 			<BlogList
 				blogs={ blogs }
-				title={ title }
 				deleteBlog={ deleteBlog }
 			/>
 		</div>
