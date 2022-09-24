d8eef7ac3df707e94b887db694f1335a827c9131
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-09-24 22:48:57 +0100

    Single post page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,33 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 const Blog = () => {
 	const { id } = useParams();
+	const {
+		data: blog,
+		loading: postLoading,
+		error: postError
+	} = useFetch(`https://jsonplaceholder.typicode.com/posts/${ id }`);
+	const {
+		data: author,
+		loading: authorLoading,
+		error: authorError
+	} = useFetch(`https://jsonplaceholder.typicode.com/users/${ blog.userId }`);
 
 	return (
 		<div className="blog-details">
-			<h2>Blog { id } Details</h2>
+			{ postLoading ? <i>loading... </i> : null }
+			{ postError ?
+				<div className="fetch-error">{ postError }</div> :
+				<article>
+					<h2>{ blog.title }</h2>
+					{ authorError ?
+						<div className="fetch-error">{ authorError }</div> :
+						<p>Author: { authorLoading ? <i>loading... </i> : author.name }</p>
+					}
+					<div>{ blog.body }</div>
+				</article>
+			}
 		</div>
 	)
 }
