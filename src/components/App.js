f6727a5371ecaa1f31a282d9046a787200cf6277
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-09-24 15:09:41 +0100

    Added react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Footer from "./Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
